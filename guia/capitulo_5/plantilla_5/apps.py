e7d6387a3bc4a4850124f5be1f7820e00ba4f139
David <overdavid.odlm@gmail.com>
2020-09-17 17:28:09 +0200

    Ejercicio 5
    Registro insertado

---
+++ guia/capitulo_5/plantilla_5/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Plantilla5Config(AppConfig):
+    name = 'plantilla_5'
---
+++ guia/capitulo_5/plantilla_5/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse
+import psycopg2
+
+def insert(request):
+    conn = psycopg2.connect(dbname="capitulo_4_db",
+                            user="capitulo_4_user",
+                            password="patata")
+    # with open("debug.log", "a+") as debug_file:
+    #     print("Funciona!", file=debug_file)
+    # return HttpResponse('Funciona!')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO emp VALUES ('7364','HUGO','OFICINISTA', \
+                   '7903',date '1980-12-17','800.00',NULL,'20');")
+    return HttpResponse('Registro insertado')
+
