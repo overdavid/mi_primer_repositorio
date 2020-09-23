263b7acae2797cf61459235b275b90525e31fb60
David <overdavid.odlm@gmail.com>
2020-09-23 16:59:00 +0200

    Ej.6 Finalizado

--- guia/capitulo_6/listas/views.py
+++ guia/capitulo_6/listas/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-import psycopg2
+import psycopg2.extras
 
 def formulario(request):
     conn = psycopg2.connect(dbname="capitulo_6_db",
                             user="capitulo_6_user",
                             password="patata")
-    cursor = conn.cursor()
-
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute("SELECT * FROM Nota;")
-    result = cursor.fetchall()
+    resultado = cursor.fetchall()
     cursor.close()
     conn.close()
-    params = {'notas': 'funciona'}
+    params = {'notas':resultado}
     return render(request, 'home6.html', params)
 
 def insert(request):
@@ -20,13 +19,12 @@ def insert(request):
     conn = psycopg2.connect(dbname="capitulo_6_db",
                             user="capitulo_6_user",
                             password="patata")
-
     prioridad = request.POST["name_prioridad"]
     titulo = request.POST["nombre_titulo"]
     nota = request.POST["name_nota"]
 
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO nota VALUES ('{prioridad}','{titulo}','{notas}');")
+    cursor.execute(f"INSERT INTO nota VALUES ('{prioridad}','{titulo}','{nota}');")
     conn.commit()
     cursor.close()
     conn.close()
