dedbcfefc8c839b5c7cfb1b49f8096b2b14897b4
David <overdavid.odlm@gmail.com>
2020-10-05 19:53:08 +0100

    Examen Capitulo 5 insertado

--- examen/proyecto_examen/app_examen/views.py
+++ examen/proyecto_examen/app_examen/views.py
@@ -4,15 +4,28 @@ import psycopg2
 
 def home_page_view(request):
    return render(request, 'examen.html')
+   return render(request, 'examen.html')
 
 def django(request):
    return render (request, 'django.html')
 
+
 def insert(request):
-    conn = psycopg2.connect(dbname="capitulo_4_db",
-                        	user="capitulo_4_user",
+	conn = psycopg2.connect(dbname="capitulo_6_db",
+                        	user="capitulo_6_user",
                         	password="patata")
-    with open("debug.log", "a+") as debug_file:
-        print("Funciona!", file=debug_file)
-    return HttpResponse('Funciona!')
+
+	cursor = conn.cursor()
+     cursor.execute("INSERT INTO emp VALUES        ('7365','HUGO','OFICINISTA', \
+'7903',date    '1980-12-17','800.00',NULL,'20');")
+	return HttpResponse("Insertado")
+
+def select(request):
+	conn = psycopg2.connect(dbname="capitulo_6_db",
+                        	user="capitulo_6_user",
+                        	password="patata")
+	cursor = conn.cursor()
+	cursor.execute("select * from emp")
+	return HttpResponse(cursor.fetchall())
+
 
